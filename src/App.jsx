0ebc1fe265241a9ac29abbfd6c98c8d0e8c229c8
shishirdubey24 <shishirdubey221@gmail.com>
2025-03-15 21:09:30 +0530

    Projct setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+      <p className="">Your setup is correct! 🚀</p>
+      <h1 className="bg-blue-500">ajdnguifhng</h1>
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+   </>
+  )
+}
+
+export default App
