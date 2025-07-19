29eb1415ed95b8d6b20ccc54473d3f310cc2272c
shishirdubey24 <shishirdubey221@gmail.com>
2025-07-19 16:22:06 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,12 @@ import Certifications from './components/Certifications'
 import ContactDetails from './components/Contact'
 import Education from './components/Education'
 import Header from './components/Header'
-import Projects from './components/Projects'
 import Skill from './components/Skill'
-
+import Projects from './components/Projects'
 function App() {
   return (
    <>
-   <div >
+   <div className="min-h-screen bg-gray-50 dark:bg-gray-900"> 
    <Header/>
     <About/>
    <Skill/>
