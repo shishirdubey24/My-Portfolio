c2ed48bcba18dab2fd4348c4071b0d91851581a2
shishirdubey24 <shishirdubey221@gmail.com>
2025-07-23 20:59:03 +0530

    Updated global csss

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Projects from './components/Projects'
 function App() {
   return (
    <>
-   <div className="min-h-screen bg-gray-50 dark:bg-gray-900"> 
+   <div className="min-h-screen "> 
    <Header/>
     <About/>
    <Skill/>
