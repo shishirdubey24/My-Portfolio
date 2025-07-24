a2c10744653cfc92e905bb9600473d894c755817
shishirdubey24 <shishirdubey221@gmail.com>
2025-07-24 14:04:23 +0530

    Updated IMports

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ import ContactDetails from './components/Contact'
 import Education from './components/Education'
 import Header from './components/Header'
 import Projects from './components/Projects'
-import Skill from './Components/Skill'
+//import Skill from './Components/Skill'
+import Skill from "./components/Skill"
 function App() {
   return (
    <>
      <div className="bg-gradient-to-b from-[#0a192f]  via-[#1e293b] to-[#1f2937] min-h-screen text-white">
    <Header/>
     <About/>
-    <Skill/>
+   <Skill/>
    <Projects/>
     <Education/>
      <Certifications/>
